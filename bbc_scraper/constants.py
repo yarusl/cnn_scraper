0f6ba6da7ae9ba3472d1a9bcf7273d7bb84f68dc
Ruslan Yakushev <rusikyakushev@yandex.ru>
2021-03-29 10:18:38 +0300

    del test info

--- bbc_scraper/constants.py
+++ bbc_scraper/constants.py
@@ -2,5 +2,3 @@ BBC_NEWS = 'bbc.com/news'
 BBC_PROTOCOL = 'https://www.bbc.com'
 SILENT_MODE = 0
 INTERACTIVE_MODE = 1
-APIKEY = "78df6bdb62e040fb886ed3b71ead4fd2"
-
--- bbc_scraper/dict.py
+++
@@ -1,6 +0,0 @@
-import json
-with open('data.json', 'r') as outfile:
-    data = json.load(outfile)
-
-print(data)
-print(data['articles'][0])
